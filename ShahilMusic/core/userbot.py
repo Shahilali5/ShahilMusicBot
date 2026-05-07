8fa2476eec09c95d7759e223d0867eb89db1c691
CODEWITHSHAHIL <110395532+Shahilali5@users.noreply.github.com>
2026-05-07 12:56:15 +0530

    Update userbot.py

--- ShahilMusic/core/userbot.py
+++ ShahilMusic/core/userbot.py
@@ -94,7 +94,7 @@ class Userbot(Client):
             await self.two.start()
             try:
                 await self.two.join_chat("ChattingClub007")
-                await self.one.join_chat("Shahil440")
+                await self.two.join_chat("Shahil440")
             except:
                 pass
             assistants.append(2)
@@ -115,7 +115,7 @@ class Userbot(Client):
             await self.three.start()
             try:
                 await self.three.join_chat("DevilWebszchat")
-                await self.one.join_chat("Shahil440")
+                await self.three.join_chat("Shahil440")
             except:
                 pass
             assistants.append(3)
@@ -136,7 +136,7 @@ class Userbot(Client):
             await self.four.start()
             try:
                 await self.four.join_chat("DevilWebszchat")
-                await self.one.join_chat("Shahil440")
+                await self.four.join_chat("Shahil440")
             except:
                 pass
             assistants.append(4)
@@ -157,7 +157,7 @@ class Userbot(Client):
             await self.five.start()
             try:
                 await self.five.join_chat("Shahil440")
-                await self.one.join_chat("DevilWebszchat")
+                await self.five.join_chat("DevilWebszchat")
             except:
                 pass
             assistants.append(5)
